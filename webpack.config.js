71baeb26f62c854e06063e22af16a6fac01632c1
U-SYNOPSYS\yeqing <yeqing@YEQING-E7450.internal.synopsys.com>
2018-06-05 16:40:16 -0700

    Usage History API working

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    // entry: './src/main/js/app.js',
+    entry: './src/main/js/diskapp.js',
+    devtool: 'sourcemaps',
+    cache: true,
+    debug: true,
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/built/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
